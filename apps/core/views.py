0b7e398c7437a942f76006015af98f47f4dc16d1
Joseph Misiti <joseph.misiti@gmail.com>
2016-01-30 13:46:37 -0500

    adding https

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,73 +1,77 @@
 from django.shortcuts import render
-from django.core.mail import EmailMultiAlternatives	
+from django.core.mail import EmailMultiAlternatives 
 from django.template.loader import render_to_string
 
+
+def lets_crypt_verification(request):
+    return render(request, 'core/https.html', {})
+    
 def index(request):
-	return render(request, 'core/index.html', {})
-	
+    return render(request, 'core/index.html', {})
+    
 def about(request):
-	return render(request, 'core/about.html', {})
-	
+    return render(request, 'core/about.html', {})
+    
 def branding_1(request):
-	return render(request, 'core/branding_1.html', {})
-	
+    return render(request, 'core/branding_1.html', {})
+    
 def branding_2(request):
-	return render(request, 'core/branding_2.html', {})
-	
+    return render(request, 'core/branding_2.html', {})
+    
 def branding_3(request):
-	return render(request, 'core/branding_3.html', {})
-	
+    return render(request, 'core/branding_3.html', {})
+    
 def capabilities(request):
-	return render(request, 'core/capabilities.html', {})
-	
+    return render(request, 'core/capabilities.html', {})
+    
 def clients(request):
-	return render(request, 'core/clients.html', {})
-	
+    return render(request, 'core/clients.html', {})
+    
 def contact(request):
-	
-	if request.method == "POST":
-				
-		html = "<p> %s </p>" % request.REQUEST.get("name")
-		html += "<p> %s </p>" % request.REQUEST.get("email")
-		html += "<p> %s </p>" % request.REQUEST.get("comment")
-		message = EmailMultiAlternatives("Math & Pencil Request Form", 
-										 html, 
-										 "noreply@mathandpencil.co,",
-										 to=settings.TO_EMAIL_LIST,
-										 bcc=[])
-		message.attach_alternative(html , 'text/html')		
-		message.send()
-		
-		print('sent ...')
-	
-	return render(request, 'core/contact.html', {})
-	
+    
+    if request.method == "POST":
+                
+        html = "<p> %s </p>" % request.REQUEST.get("name")
+        html += "<p> %s </p>" % request.REQUEST.get("email")
+        html += "<p> %s </p>" % request.REQUEST.get("comment")
+        message = EmailMultiAlternatives("Math & Pencil Request Form", 
+                                         html, 
+                                         "noreply@mathandpencil.co,",
+                                         to=settings.TO_EMAIL_LIST,
+                                         bcc=[])
+        message.attach_alternative(html , 'text/html')      
+        message.send()
+        
+        print('sent ...')
+    
+    return render(request, 'core/contact.html', {})
+    
 def projects(request):
-	return render(request, 'core/projects.html', {})
-	
+    return render(request, 'core/projects.html', {})
+    
 def projects_1(request):
-	return render(request, 'core/projects_1.html', {})
-	
+    return render(request, 'core/projects_1.html', {})
+    
 def projects_2(request):
-	return render(request, 'core/projects_2.html', {})
-	
+    return render(request, 'core/projects_2.html', {})
+    
 def projects_3(request):
-	return render(request, 'core/projects_3.html', {})
-	
+    return render(request, 'core/projects_3.html', {})
+    
 def projects_4(request):
-	return render(request, 'core/projects_4.html', {})
-	
+    return render(request, 'core/projects_4.html', {})
+    
 def team(request):
-	return render(request, 'core/team.html', {})
-	
+    return render(request, 'core/team.html', {})
+    
 def test(request):
-	return render(request, 'core/test.html', {})
-	
+    return render(request, 'core/test.html', {})
+    
 def websites_1(request):
-	return render(request, 'core/websites_1.html', {})
-	
+    return render(request, 'core/websites_1.html', {})
+    
 def websites_2(request):
-	return render(request, 'core/websites_2.html', {})
-	
+    return render(request, 'core/websites_2.html', {})
+    
 def websites_3(request):
-	return render(request, 'core/websites_3.html', {})
+    return render(request, 'core/websites_3.html', {})
--- mathandpencil/urls.py
+++ mathandpencil/urls.py
@@ -4,24 +4,27 @@ from django.contrib.auth import views as auth_views
 
 
 urlpatterns = patterns('',
-	
-	url(r'^$', 								'apps.core.views.index',			name='index'),
-	url(r'^about$', 						'apps.core.views.about',			name='about'),
-	url(r'^capabilities$',				 	'apps.core.views.capabilities',	name='capabilities'),
-	url(r'^clients$',				 		'apps.core.views.clients',		name='clients'),
-	url(r'^contact$', 						'apps.core.views.contact',		name='contact'),
-	url(r'^projects$', 						'apps.core.views.projects',		name='projects'),
-	url(r'^team$', 							'apps.core.views.team',			name='team'),
-	url(r'^test$', 							'apps.core.views.test',			name='test'),
-	
-	url(r'^projects/fetcher$', 				'apps.core.views.projects_4',		name='projects_4'),
-	url(r'^projects/dukemail$', 			'apps.core.views.projects_3',		name='projects_3'),
-	url(r'^projects/employii$', 			'apps.core.views.projects_2',		name='projects_2'),
-	url(r'^projects/socialq$', 				'apps.core.views.projects_1',		name='projects_1'),
-	
-	url(r'^branding/business-cards$', 		'apps.core.views.branding_1',		name='branding_1'),
-	url(r'^branding/identity-packages$',	'apps.core.views.branding_2',		name='branding_2'),
-	)
+    
+    url(r'^$',                              'apps.core.views.index',            name='index'),
+    url(r'^about$',                         'apps.core.views.about',            name='about'),
+    url(r'^capabilities$',                  'apps.core.views.capabilities', name='capabilities'),
+    url(r'^clients$',                       'apps.core.views.clients',      name='clients'),
+    url(r'^contact$',                       'apps.core.views.contact',      name='contact'),
+    url(r'^projects$',                      'apps.core.views.projects',     name='projects'),
+    url(r'^team$',                          'apps.core.views.team',         name='team'),
+    url(r'^test$',                          'apps.core.views.test',         name='test'),
+    
+    url(r'^projects/fetcher$',              'apps.core.views.projects_4',       name='projects_4'),
+    url(r'^projects/dukemail$',             'apps.core.views.projects_3',       name='projects_3'),
+    url(r'^projects/employii$',             'apps.core.views.projects_2',       name='projects_2'),
+    url(r'^projects/socialq$',              'apps.core.views.projects_1',       name='projects_1'),
+    
+    url(r'^branding/business-cards$',       'apps.core.views.branding_1',       name='branding_1'),
+    url(r'^branding/identity-packages$',    'apps.core.views.branding_2',       name='branding_2'),
+    
+    url(r'^.well-known/acme-challenge/AVqk3G4JU_YvsxXMvfZuBPzT9YHLVZoPQr2JiK3h3gQ', 'apps.core.views.lets_crypt_verification')
+    
+    )
 
 if settings.DEBUG:
     urlpatterns += patterns('',
